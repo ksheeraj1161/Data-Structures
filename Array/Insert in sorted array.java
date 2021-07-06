1ce92bcbc2029769b1b010f51ad5565d378cd438
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-07-06 13:55:01 +0530

    Create Insert in sorted array.java

---
+++ Array/Insert in sorted array.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    static int insertSorted(int arr[], int n, int key, int capacity)
+    {
+
+        if (n >= capacity)
+            return n;
+
+        int i;
+        for (i = n - 1; (i >= 0 && arr[i] > key); i--)
+            arr[i + 1] = arr[i];
+
+        arr[i + 1] = key;
+
+        return (n + 1);
+    }
+
+    public static void main(String[] args)
+    {
+        int arr[] = new int[20];
+        arr[0] = 12;
+        arr[1] = 16;
+        arr[2] = 20;
+        arr[3] = 40;
+        arr[4] = 50;
+        arr[5] = 70;
+        int capacity = arr.length;
+        int n = 6;
+        int key = 26;
+
+        System.out.print("\nBefore Insertion: ");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+
+        n = insertSorted(arr, n, key, capacity);
+
+        System.out.print("\nAfter Insertion: ");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
