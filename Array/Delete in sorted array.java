27a80a47d9abe7675c3971a2495aee209bf9ab0d
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-07-06 13:55:22 +0530

    Create Delete in sorted array.java

---
+++ Array/Delete in sorted array.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    static int binarySearch(int arr[], int low, int high, int key)
+    {
+        if (high < low)
+            return -1;
+        int mid = (low + high) / 2;
+        if (key == arr[mid])
+            return mid;
+        if (key > arr[mid])
+            return binarySearch(arr, (mid + 1), high, key);
+        return binarySearch(arr, low, (mid - 1), key);
+    }
+
+    static int deleteElement(int arr[], int n, int key)
+    {
+        
+        int pos = binarySearch(arr, 0, n - 1, key);
+
+        if (pos == -1) {
+            System.out.println("Element not found");
+            return n;
+        }
+
+     
+        int i;
+        for (i = pos; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+
+        return n - 1;
+    }
+
+    public static void main(String[] args)
+    {
+
+        int i;
+        int arr[] = { 10, 20, 30, 40, 50 };
+
+        int n = arr.length;
+        int key = 30;
+
+        System.out.print("Array before deletion:\n");
+        for (i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+
+        n = deleteElement(arr, n, key);
+
+        System.out.print("\n\nArray after deletion:\n");
+        for (i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
